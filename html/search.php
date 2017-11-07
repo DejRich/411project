b46b4d82e891264c6bd5e9b244b810a5e0804e05
DejRich <genoxc@gmail.com>
2017-11-06 19:59:32 -0600

    add general query

--- html/search.php
+++ html/search.php
@@ -3,7 +3,13 @@
     <?php
     include 'resources.php';
     $input = "\"%" . $_POST["search_box"] . "%\"";
-    $query = "SELECT * FROM business WHERE name LIKE  " . $input;
+    $isGeneralQuery = $_POST["isGeneralQuery"];
+    if($isGeneralQuery){
+        $query = $_POST["search_box"];
+    }
+    else{
+        $query = "SELECT * FROM business WHERE name LIKE " . $input;
+    }
     //Now you should be able to search a restaurant by name. --Shen
     $result = query($query);
     printSqlResults($result);
