dd98742dc52218a3db51dc1ba677ad064e2daa55
DejRich <genoxc@gmail.com>
2017-11-11 18:05:21 -0600

    update rest

--- html/restaurant.php
+++ html/restaurant.php
@@ -10,8 +10,22 @@
     ?>
 
     <h2> <?= $row["name"] ?> </h2>
-    <p> hhh </p>
-
-
+    <p>Stars: <?= $row["stars"] ?> </p>
+    <p>Address: <?= $row["address"] ?>, <?= $row["city"] ?>, <?= $row["state"] ?></p>
+    
+    <hr>
+    <h4>Reviews:</h4>
+    
+    <?php
+    $query = "SELECT * FROM review, user u WHERE user_id = u.id AND business_id=\"" . $id . "\"";
+    $res = query($query);
+    while($row = $res->fetch_assoc()){
+        ?>
+        <hr>
+        <h4><?= $row["name"] ?> StarsL <?= $row["star"] ?> Date: <?= $row["date"] ?></h4>
+        <p><?= $row["text"] ?></p>
+       <?php 
+    }
+        ?>
     </body>
 </html>
